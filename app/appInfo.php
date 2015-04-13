1c7345dfaa96ca715eb56ace4b1ebe29628023f3
Igor Carvalho <igor822@gmail.com>
2015-04-13 00:01:23 -0300

    Updated info

--- app/appInfo.php
+++ app/appInfo.php
@@ -7,7 +7,8 @@ return <<<EOL
     <info>list</info>\t List all stored tasks
     <info>add</info>\t\t Add a new task
     <info>toggle</info>\t Toggles task status
-    <info>remove</info>\t Remove the task\n
+    <info>remove</info>\t Remove the task
     <info>update</info>\t Self update todo app
+
 The <info>todo</info> you can be able to manage your tasks from your terminal.
 EOL;
