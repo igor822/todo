759137a80c2ed1c966db563efcefe43910737a3b
Igor Carvalho <igor822@gmail.com>
2015-04-19 20:21:11 -0300

    Added static method to create a directory

--- src/Component/PathnameBuilder.php
+++ src/Component/PathnameBuilder.php
@@ -23,15 +23,32 @@ class PathnameBuilder
 
     /**
      * @param string $pathname
+     * @return string
      */
     public function replacePathHome($pathname)
     {
         $this->path = str_replace(PathPattern::HOME, getenv('HOME'), $pathname);
+        return $this->path;
     }
 
     public function createDir($path)
     {
         mkdir($path);
+    }
+
+    public static function createDirectory($path, $mode = 0777)
+    {
+        $class = __CLASS__;
+        /** @var PathnameBuilder $instance */
+        $instance = new $class($path);
+        if ($instance->hasHome()) {
+            $path = $instance->replacePathHome($path);
+        }
+
+        if (!is_dir($path)) {
+            mkdir($path, $mode);
+        }
+        return $instance;
     }
 
     public function checkAlternatives()
@@ -57,5 +74,10 @@ class PathnameBuilder
 
         $fullPath = sprintf('%s/%s', $this->path, $filename);
         return $fullPath;
+    }
+
+    public static function getHomePath()
+    {
+        return getenv('HOME');
     }
 }
