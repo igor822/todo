72f8a5099c091d15cb38dbec77d0da25872296d0
Igor Carvalho <igor822@gmail.com>
2015-04-19 20:01:31 -0300

    Added informations to help content

--- app/appInfo.php
+++ app/appInfo.php
@@ -4,11 +4,13 @@ return <<<EOL
 <comment>Usage:</comment>
     todo [command] [content]\n
 <comment>Command:</comment>
-    <info>list</info>\t List all stored tasks
-    <info>add</info>\t\t Add a new task
-    <info>toggle</info>\t Toggles task status
-    <info>remove</info>\t Remove the task
-    <info>update</info>\t Self update todo app
+    <info>list</info>\tList all stored tasks
+    <info>search</info>\t[query]\t\t Search by hash tag on tasks contents
+    <info>add</info>\t\t[content]\t Add a new task
+    <info>toggle</info>\t[id]\t\t Toggles task status
+    <info>config</info>\t[option]\t Set configuration of app
+    <info>remove</info>\t[id]\t\t Remove the task
+    <info>update</info>\tSelf update todo app
 
-The <info>todo</info> you can be able to manage your tasks from your terminal.
+The <info>todo</info> you can be able to manage your tasks from your terminal.\n
 EOL;
