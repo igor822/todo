6fb9a9345e064493211250da3e38840a87caa007
Igor Carvalho <igor822@gmail.com>
2015-04-21 00:02:12 -0300

    Updated help content and version number

--- app/appInfo.php
+++ app/appInfo.php
@@ -5,10 +5,11 @@ return <<<EOL
     todo [command] [content]\n
 <comment>Command:</comment>
     <info>list</info>\tList all stored tasks
+    <info>config</info>\t[option]\t Set configuration of app
     <info>search</info>\t[query]\t\t Search by hash tag on tasks contents
     <info>add</info>\t\t[content]\t Add a new task
+    <info>update</info>\t[id] [value]\t Update a task
     <info>toggle</info>\t[id]\t\t Toggles task status
-    <info>config</info>\t[option]\t Set configuration of app
     <info>remove</info>\t[id]\t\t Remove the task
     <info>self-update</info>\tUpdate to current version of todo app
 
